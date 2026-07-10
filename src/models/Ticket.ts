f0c52d46325f58a501e92f12d70768aa5c2e6080
JustinDouglas16 <justinrafael.douglas@gmail.com>
2026-07-10 07:52:08 -0300

    added ticket interface

--- src/models/Ticket.ts
+++ src/models/Ticket.ts
@@ -5,3 +5,15 @@ export type TicketPriority = "low" | "normal" | "high" | "critical";
 // ticket status can be set for tickets
 
 export type TicketStatus = "open" | "in-progress" | "closed";
+
+// ticket interface serves as a blueprint on what a ticket should have
+
+export interface Ticket {
+  id: string;
+  title: string;
+  description: string;
+  priority: TicketPriority;
+  status: TicketStatus;
+  createdAt: Date;
+  resolvedAt: Date | null;
+}
