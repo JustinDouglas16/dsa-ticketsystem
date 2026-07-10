ac2211a90a83d10088b42703b7e7207a2493773a
JustinDouglas16 <justinrafael.douglas@gmail.com>
2026-07-10 11:20:01 -0300

    added some simple validation checks and comments

--- src/utils/createTicket.ts
+++ src/utils/createTicket.ts
@@ -8,10 +8,29 @@ interface CreateTicketInput {
 }
 
 export function createTicket(input: CreateTicketInput): Ticket {
+  // some simple validation checks for the ticket
+  // In JavaScript, the String.prototype.trim() method removes whitespace from both ends of a string and returns a brand-new string without modifying the original one
+  const cleanId = input.id.trim();
+  const cleanTitle = input.title.trim();
+  const cleanDescription = input.description.trim();
+
+  //   TODO: need to look into writing this more concise
+  if (cleanId.length === 0) {
+    throw new Error("Een ticket moet een ID hebben.");
+  }
+
+  if (cleanTitle.length === 0) {
+    throw new Error("Een ticket moet een titel hebben.");
+  }
+
+  if (cleanDescription.length === 0) {
+    throw new Error("Een ticket moet een beschrijving hebben.");
+  }
+
   return {
-    id: input.id,
-    title: input.title,
-    description: input.description,
+    id: cleanId,
+    title: cleanTitle,
+    description: cleanDescription,
     priority: input.priority,
     status: "open",
     createdAt: new Date(),
