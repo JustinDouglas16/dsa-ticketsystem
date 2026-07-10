a65f85d972a567533d8faedead88655369f031a5
JustinDouglas16 <justinrafael.douglas@gmail.com>
2026-07-10 07:46:23 -0300

    added ticket status

--- src/models/Ticket.ts
+++ src/models/Ticket.ts
@@ -1,3 +1,7 @@
 // ticket priority would only have one of these
 
 export type TicketPriority = "low" | "normal" | "high" | "critical";
+
+// ticket status can be set for tickets
+
+export type TicketStatus = "open" | "in-progress" | "closed";
